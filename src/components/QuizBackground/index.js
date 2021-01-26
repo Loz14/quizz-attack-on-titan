faed34a86233cbb06148286891db848feef3fe08
Luiz André Carvalho <luizandre.ita@gmail.com>
2021-01-26 17:26:19 -0300

    ajustes pro mobile

--- src/components/QuizBackground/index.js
+++ src/components/QuizBackground/index.js
@@ -10,7 +10,8 @@ const QuizBackground = styled.div`
   background-color: ${({ theme }) => theme.colors.mainBg};
   @media screen and (max-width: 500px) {
     width: 100%;
-    background-size: auto;
+    overflow-y: hidden;
+    background-size: cover;
     background-position: center;
     background-color: ${({ theme }) => theme.colors.mainBg};
     background-image: url(https://fsa.zobj.net/crop.php?r=jUKrDc9yC5TGBB4VeN7k8RKhL24PvkDlzE2WS5rFOBAxq6hOVJW7AcZ4-pkhCL-72X4L7C6TAKO0ipAEUYeNGNdgLjRvWOVra0Yu-80b0MtK3Z0J3-jqlmqPCdz69W7MThbjXetO0iafytoI);
