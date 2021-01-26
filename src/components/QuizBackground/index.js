cbd3b4960106c88bda42b02e8083368d12da1e92
Luiz André Carvalho <luizandre.ita@gmail.com>
2021-01-26 17:16:46 -0300

    image dynamic

--- src/components/QuizBackground/index.js
+++ src/components/QuizBackground/index.js
@@ -2,11 +2,18 @@
 import styled from 'styled-components';
 
 const QuizBackground = styled.div`
-  width: 100%;
+  width: 100vw;
+  height: 100vh;
   background-size: cover;
   background-position: center;
   background-image: url(${({ backgroundImage }) => backgroundImage});
   background-color: ${({ theme }) => theme.colors.mainBg};
+  @media screen and (max-width: 500px) {
+    width: 100%;
+    background-size: cover;
+    background-position: center;
+    background-color: ${({ theme }) => theme.colors.mainBg};
+    background-image: url(https://fsa.zobj.net/crop.php?r=jUKrDc9yC5TGBB4VeN7k8RKhL24PvkDlzE2WS5rFOBAxq6hOVJW7AcZ4-pkhCL-72X4L7C6TAKO0ipAEUYeNGNdgLjRvWOVra0Yu-80b0MtK3Z0J3-jqlmqPCdz69W7MThbjXetO0iafytoI);
   flex: 1;
     *:first-child {
       position: relative;
