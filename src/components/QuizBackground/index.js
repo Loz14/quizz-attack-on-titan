a0f609d8f7f3b6ed5c5f27db882dd3994b99132c
Luiz André Carvalho <luizandre.ita@gmail.com>
2021-01-26 17:32:57 -0300

    overflow

--- src/components/QuizBackground/index.js
+++ src/components/QuizBackground/index.js
@@ -4,13 +4,14 @@ import styled from 'styled-components';
 const QuizBackground = styled.div`
   width: 100vw;
   height: 100vh;
+  overflow: hidden;
   background-size: cover;
   background-position: center;
   background-image: url(${({ backgroundImage }) => backgroundImage});
   background-color: ${({ theme }) => theme.colors.mainBg};
   @media screen and (max-width: 500px) {
     width: 100%;
-    overflow-y: hidden;
+    overflow: hidden;
     background-size: cover;
     background-position: center;
     background-color: ${({ theme }) => theme.colors.mainBg};
